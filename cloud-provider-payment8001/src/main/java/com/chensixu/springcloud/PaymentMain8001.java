943a322787714807bb5548ff98fb865556f7582f
EventDHorizon <912987200@qq.com>
2023-02-01 00:02:24 +0800

    feat：添加支付服务

---
+++ cloud-provider-payment8001/src/main/java/com/chensixu/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.chensixu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chensixu
+ * @description
+ * @date 2023/1/31 23:49
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+
+}
